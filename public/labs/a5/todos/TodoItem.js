ca407503efecffee26d4362d3de486051c43f8f9
Vaishnavishah <shahvaishnavi2411@gmail.com>
2023-02-17 18:34:54 -0500

    Implemented TODOs

---
+++ public/labs/a5/todos/TodoItem.js
@@ -0,0 +1,12 @@
+const TodoItem = (todo) => {
+   return(`
+       <input type="checkbox"
+               ${todo.done ? 'checked' : ''}/>
+             ${todo.title}
+             (${todo.status})
+
+
+   `);
+}
+export default TodoItem;
+

4e537032d8870a613042c3df0d76cb036ac55426
Vaishnavishah <shahvaishnavi2411@gmail.com>
2023-02-18 17:28:07 -0500

    Implemented active for nav side bar and top nav bar

--- public/tuiter/navigationSidebar/index.js
+++ public/tuiter/navigationSidebar/index.js
@@ -1,3 +1,54 @@
+const navItems = [
+  {
+    link: "#",
+    title: "Home",
+    icon: "fa fa-home",
+    active: false,
+  },
+  {
+    link: "#",
+    title: "Explore",
+    icon: "fa fa-hashtag",
+    active: true,
+  },
+  {
+    link: "#",
+    title: "Notifications",
+    icon: "fa fa-bell",
+    active: false,
+  },
+  {
+    link: "#",
+    title: "Messages",
+    icon: "fa fa-envelope",
+    active: false,
+  },
+  {
+    link: "#",
+    title: "Bookmarks",
+    icon: "fa fa-bookmark",
+    active: false,
+  },
+  {
+      link: "#",
+      title: "Lists",
+      icon: "fa fa-list",
+      active: false,
+    },
+    {
+        link: "#",
+        title: "Profiles",
+        icon: "fa fa-user",
+        active: false,
+     },
+     {
+         link: "#",
+         title: "More",
+         icon: "fa fa-circle",
+         active: false,
+      },
+];
+
 const NavigationSidebar = () => {
  return(`
    <div class="list-group">
@@ -6,47 +57,18 @@ const NavigationSidebar = () => {
        <!-- continue rest of list, e.g.,
             Home, Explore, Notifications,  Messages, etc. -->
 
-     <a class="list-group-item" href="/">
-        <i class="fa fa-home"></i>
-        Home
-     </a>
-     <a class="list-group-item active" href="/">
-        <i class="fa fa-hashtag"></i>
-        Explore
-     </a>
-
-     <a class="list-group-item" href="/">
-         <i class="fa fa-bell"></i>
-         Notifications
-     </a>
-
-      <a class="list-group-item" href="/">
-           <i class="fa fa-envelope"></i>
-           Messages
-      </a>
-
-      <a class="list-group-item" href="/">
-         <i class="fa fa-bookmark"></i>
-               Bookmarks
-
-      </a>
 
 
-      <a class="list-group-item" href="/">
-          <i class="fa fa-list"></i>
-                     Lists
-      </a>
+                               ${navItems.map(
+                                          (item) => `<a href=${item.link}  class = ${`"list-group-item ${
+                                          item.active ? `active` : ``
+                                          }"`}>
+                                          <i class = "${item.icon}"></i>
+                                          ${item.title}
+                                          </a>`
+                               ).join("")}
 
-      <a class="list-group-item" href="/">
-           <i class="fa fa-user"></i>
-                           Profiles
 
-      </a>
-
-      <a class="list-group-item" href="/">
-           <i class="fa fa-circle"></i>
-           More
-      </a>
 
    </div>
    <div class="d-grid mt-2">
